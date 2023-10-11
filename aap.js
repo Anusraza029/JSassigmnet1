fefc680918997356289c65add7e2039a6160acad
Anusraza09 <sanus8339@gmail.com>
2023-10-11 08:24:18 -0700

    github.com/Anusraza029/JSassigmnet1.git

---
+++ aap.js
@@ -0,0 +1,16 @@
+alert ("Error! Please inter a valid password.");
+
+alert ("Welcome to JSLand...\nHappy Coding!");
+
+alert ("Welcome to JSLand...");
+
+alert ("Happy Coding! \nPrevent this page from creating additional dialogs.")
+
+var thisName = "Hello... I can JS through my web browser's console"
+console.log(thisName)
+
+alert ("Hello... I can JS through my web browser's console")
+
+document.write ("Hello... I can JS through my web browser's console")
+
+
